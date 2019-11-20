071f53641db71280bf8ad77f488d695d75ebe1b3
Joseph Keogh <jgk7uf@virginia.edu>
2019-11-20 16:16:05 -0500

    Automatically making commit for J

--- upload.sh
+++ upload.sh
@@ -1,4 +1,42 @@
-pip3 install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 8000
+#!/bin/bash
+
+# get the user's name
+echo "Please enter your name..."
+read userName
+
+# the current date, to be used in push/pull history
+now=`date`
+
+# message to user
+echo Thank You $userName
+echo
+
+	# message to user
+	echo "You have made changes to the files..."
+	echo
+	echo "Saving your changes to the Cloud..."
+	echo
+	
+	# Update the push history to have the user's name and the current time
+	echo >> push-history.txt
+	echo "$now" >> push-history.txt
+	echo "$userName" >> push-history.txt
+
+	# push the user's changes and store info
+	git add . >> push-history.txt
+	git commit -m "Automatically making commit for $userName" >> push-history.txt
+	git push >> push-history.txt
+
+	# message to user
+	echo "Changes saved to the Cloud."
+	
+	# store the changes to the push history in the repository
+	git add . >> trash.txt
+	git commit -m "updating push history" >> trash.txt
+	git push >> trash.txt
+	
+rm difference.txt
+rm test.txt
+rm trash.txt
+
+echo Finished
