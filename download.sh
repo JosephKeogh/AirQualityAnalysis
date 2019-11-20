30207c859ab222d566a3cffb03a05e225fbc233b
Joseph Keogh <jgk7uf@virginia.edu>
2019-11-20 15:59:41 -0500

    Automatically making commit for J

--- download.sh
+++ download.sh
@@ -1,63 +1,82 @@
 #!/bin/bash
 
+# get the user's name
 echo "Please enter your name..."
-
 read userName
 
+# the current date, to be used in push/pull history
 now=`date`
 
+# message to user
 echo Thank You $userName
 echo
 
+# get the status of the repository
 git status >> test.txt
 
+# check if there where changes to any files by comparing status to no changes
 diff test.txt download-ready.txt >> difference.txt
 
+# if there are changes
 if [ -s difference.txt ]
 then
+	# message to user
 	echo "You have made changes to the files..."
 	echo
 	echo "Saving your changes to the Cloud..."
 	echo
 	
+	# Update the push history to have the user's name and the current time
 	echo > push-info.txt
 	echo "$now" > push-info.txt
 	echo "$userName" > push-info.txt
 
+	# push the user's changes and store info
 	git add . > push-info.txt
-	git commit -m "Automatically making commit for $userName"
+	git commit -m "Automatically making commit for $userName" > push-info.txt
 	git push > push-info.txt
 
+	# message to user
 	echo "Changes saved to the cloud"
 	echo
 	echo "Downloading files from the cloud..."
 	echo
 
+	# now that the user's changes have been saved pull from the repository and store history
 	echo > pull-history.txt
 	echo $now > pull-history.txt
 	echo $userName > pull-history.txt
 	git pull > pull-history.txt
 
-	git add . > push-info.txt
-	git commit -m "updating pull history"
-	git push > push-info.txt
+	# store the changes to the pull and push history in the repository
+	git add . > trash.txt
+	git commit -m "updating pull and push history" > trash.txt
+	git push > trash.txt
 
+	# message to user
 	echo "All files up to date with the Cloud."
 	echo
+
+# if there were no changes to the repository
 else
+	# message to user
 	echo "You have made no changes to the files."
 	echo 
 	echo "Downloading files from the cloud..."
 	echo
 
+	# put the user's name and time in the pull history
 	echo > pull-history.txt
+	echo $now > pull-history.txt
 	echo $userName > pull-history.txt
 	git pull > pull-history.txt
 
-	git add . > push-info.txt
-	git commit -m "updating pull history"
-	git push > push-info.txt	
+	# store the pull history changes in the repository
+	git add . > trash.txt
+	git commit -m "updating pull history" > trash.txt
+	git push > trash.txt
 
+	# message to user
 	echo "All files up to date with the Cloud."
 	echo
 fi
@@ -65,5 +84,6 @@ fi
 
 rm difference.txt
 rm test.txt
+rm trash.txt
 
 echo Finished
