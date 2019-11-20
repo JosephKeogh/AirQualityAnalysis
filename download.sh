bde86590ef6dda9d941267866f5c99e77ec0fe78
Joseph Keogh <jgk7uf@virginia.edu>
2019-11-20 15:27:15 -0500

    formatting

--- download.sh
+++ download.sh
@@ -5,6 +5,7 @@ echo "Please enter your name..."
 read userName
 
 echo Thank You $userName
+echo .
 
 git status >> test.txt
 
@@ -13,14 +14,18 @@ diff test.txt download-ready.txt >> difference.txt
 if [ -s difference.txt ]
 then
 	echo "you have made changes to the files..."
+	echo .
 	echo "saving the changes to the cloud..."
+	echo .
 
 	git add .
 	git commit -m "automatically making commit"
 	git push > push-info.txt
 
 	echo "changes saved to the cloud"
+	echo .
 	echo "downloading files from the cloud..."
+	echo .
 
 	echo >> pull-history.txt
 	echo $userName >> pull-history.txt
@@ -31,9 +36,12 @@ then
 	git push > push-info.txt
 
 	echo "all files are up to date with the cloud"
+	echo .
 else
 	echo "you have made no changes to the files"
+	echo .
 	echo "downloading files from the cloud..."
+	echo .
 
 	echo >> pull-history.txt
 	echo $userName >> pull-history.txt
@@ -44,8 +52,11 @@ else
 	git push > push-info.txt	
 
 	echo "all the files are up to date with the cloud"
+	echo .
 fi
 
 > difference.txt
 > test.txt
 rm push-info.txt
+
+echo Finished
