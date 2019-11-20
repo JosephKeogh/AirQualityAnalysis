ed46967aa67377d30d56f2aa162c11150dc056b1
Joseph Keogh <jgk7uf@virginia.edu>
2019-11-20 14:44:47 -0500

    down

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+>> test.txt
+
 git status >> test.txt
 
 diff test.txt download-ready.txt >> difference.txt
 
-if[ -s difference.txt ]
+if [ -s difference.txt ]
 then
 	echo "the files are different"
 else
 	echo "the files are NOT different"
 fi
-
-git pull
